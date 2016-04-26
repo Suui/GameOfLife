20f3aef5b619302e326883d253ee1643feeb0218
Suui <suui.gd@gmail.com>
2016-04-27 01:17:58 +0200

    Added ConsoleDisplay

---
+++ ConsoleDisplay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GameOfLife;
+
+
+namespace Display
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var world = new World(20, 20);
+
+			world.PlaceLivingCellIn(10, 9);
+			world.PlaceLivingCellIn(10, 10);
+			world.PlaceLivingCellIn(10, 11);
+
+			for (var index = 0; index < 100; index++)
+			{
+				Console.Clear();
+				Console.WriteLine(world.ToString());
+				world.Update();
+				System.Threading.Thread.Sleep(1000);
+			}
+				
+		}
+	}
+}
